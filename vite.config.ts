fd02bc7883c46882562e2271f877055a9073962d
ryosukehys <111954267+ryosukehys@users.noreply.github.com>
2026-03-08 09:32:07 +0900

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
   return {
-    base: '/niigata-sake-no-jin-26/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
