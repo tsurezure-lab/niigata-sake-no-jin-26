57caa0897125ce3126574b1480e1a93d6c5af1d7
ryosukehys <111954267+ryosukehys@users.noreply.github.com>
2026-03-09 14:06:05 +0900

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
   return {
-    base: '/',
+    base: '/sakenojin2026/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
